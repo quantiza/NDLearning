efbb5ac0078e45aef2aedccf761259387c2c34cd
杨龙 <yl@huancheclub.com>
2016-11-04 15:24:56 +0800

    Init 搭建基本框架，pod引入三方库，加入预编译头文件pch

---
+++ NH/NH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NH
+//
+//  Created by yangl on 2016/11/4.
+//  Copyright © 2016年 yl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
